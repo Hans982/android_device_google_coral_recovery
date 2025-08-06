1426e72d20413d2a6a54193c2f6ba4a61c30f2da
Hans982 <142167463+Hans982@users.noreply.github.com>
2025-08-06 13:28:48 +0700

    Update BoardConfig-common.mk

--- BoardConfig-common.mk
+++ BoardConfig-common.mk
@@ -249,3 +249,7 @@ BOARD_PERFSETUP_SCRIPT := platform_testing/scripts/perf-setup/c2f2-setup.sh
 
 -include vendor/google_devices/coral/proprietary/BoardConfigVendor.mk
 -include device/google/coral/twrp.mk
+
+# OrangeFox / TWRP brightness settings
+TW_MAX_BRIGHTNESS := 255
+TW_DEFAULT_BRIGHTNESS := 120
