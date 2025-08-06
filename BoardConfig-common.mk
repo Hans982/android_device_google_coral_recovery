7e3e7a41c522b117c2630381e9e73508c5d8fd10
Hans982 <142167463+Hans982@users.noreply.github.com>
2025-08-06 16:57:05 +0700

    Update BoardConfig-common.mk

--- BoardConfig-common.mk
+++ BoardConfig-common.mk
@@ -257,3 +257,6 @@ TW_DEFAULT_BRIGHTNESS := 120
 TARGET_PREBUILT_KERNEL := device/google/coral/prebuilt/kernel/kernel
 BOARD_PREBUILT_DTBIMAGE_DIR := device/google/coral/prebuilt/kernel
 BOARD_INCLUDE_DTB_IN_BOOTIMG := true
+TARGET_FORCE_PREBUILT_KERNEL := true
+TARGET_NO_KERNEL_OVERRIDE := true
+TARGET_NO_KERNEL_SOURCE := true
